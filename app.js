e5485c3d91e0dfa594093929a19350fe3ba35d43
jaxoncreed <jaxoncreed@gmail.com>
2014-11-09 22:04:35 -0500

    did everything

--- app.js
+++ app.js
@@ -1,11 +1,124 @@
+curWord = "world"
+
+var twit = require("twit");
+var Bot = require("./bot");
+var config = require("./config1");
 var rita = require("rita");
-var speakeasy = require("speakeasy-nlp");
 
-var rs = rita.RiString("The elephant took a bite.");
+var bot = new Bot(config);
 
-var answer = function(question) {
-    speakeasy.classify("What is your name?");
+var post = function() {
+    bot.searchWord(curWord, "2014-11-01", 50, function(err, data, res){
+        if (err) {
+            console.log("Error" + err);
+            return;
+        }
+        var statusArr = [];
+        var all = [];
+        var nouns = [];
+        var verbs = [];
+        var adjs = [];
+        var advs = [];
+        var other = [];
+        for (var i = 0; i < data.statuses.length; i++) {
+            statusArr[i] = rita.RiString(data.statuses[i].text.replace('#','').replace('\\',''));
+            //Take care of individual words
+            for (var j = 0; j < statusArr[i].wordCount(); j++) {
+                var pos = statusArr[i].posAt(j);
+                if (wordQualifies(statusArr[i].wordAt(j).toLowerCase())) {
+                    all[all.length] = statusArr[i].wordAt(j).toLowerCase();
+                    if (pos.slice(0,2) == "nn") {
+                        nouns[nouns.length] = statusArr[i].wordAt(j).toLowerCase();
+                    } else if (pos.slice(0,2) == "vb") {
+                        verbs[verbs.length] = statusArr[i].wordAt(j).toLowerCase();
+                    } else if (pos.slice(0,2) == "jj") {
+                        adjs[adjs.length] = statusArr[i].wordAt(j).toLowerCase();
+                    } else if (pos.slice(0,2) == "rb") {
+                        advs[advs.length] = statusArr[i].wordAt(j).toLowerCase();
+                    } else {
+                        other[other.length] = statusArr[i].wordAt(j).toLowerCase();
+                    }
+                }
+            }
+        }
+        nouns = frequentElement(nouns);
+        verbs = frequentElement(verbs);
+        adjs = frequentElement(adjs);
+        advs = frequentElement(advs);
+        adjs.splice(nouns.indexOf("good"), 1);
+
+        //Now make the sentence (noun adverb verb adjective noun)
+        var sentence = nouns[random(5)] + " " + verbs[random(5)] + " " + adjs[random(5)] + " " + nouns[random(6, 10)] + ". #" + curWord;
+        
+        //Pick the next topic
+        var rand = random(7);
+        if (rand == 7) {
+            curWord = advs[random(5)]
+        } else if (rand == 6) {
+            curWord = adjs[random(5)]
+        } else if (rand == 5 || rand == 4) {
+            curWord = verbs[random(5)]
+        } else {
+            curWord = nouns[random(5)];
+        }
+        console.log(sentence);
+        bot.tweet(sentence, function(err, parsed, res) {});
+    });
 }
 
-var args = process.argv.slice(2);
-answer(args[0]);
+setInterval(function() {
+    post();
+}, 30 * 60 * 1000);
+
+function random(a, b) {
+    if (typeof(b) == "undefined") {
+        b = 1;
+    }
+    return Math.floor((Math.random() * a) + b);
+}
+
+function wordQualifies(word) {
+    if (word.indexOf('/') > -1) {
+        return false;
+    }
+    if (word == curWord) {
+        return false;
+    }
+    if (word == 'http') {
+        return false;
+    }
+    if (word == 'rt') {
+        return false;
+    }
+    return true;
+}
+
+function frequentElement(arr) {
+    var map = {};
+    for (var i = 0; i < arr.length; i++) {
+        if (typeof(map[arr[i]]) != 'undefined') {
+            map[arr[i]]++;
+        } else {
+            map[arr[i]] = 1;
+        }
+    }
+    //Sort the words
+    var sorted = [];
+    while (Object.keys(map).length > 0) {
+        var greatest = greatestNumInMap(map);
+        delete map[greatest];
+        sorted[sorted.length] = greatest;
+    }
+    return sorted;
+}
+function greatestNumInMap(map) {
+    var mostNum = 0;
+    var mostWord = '';
+    for(key in map) {
+        if (map[key] > mostNum) {
+            mostNum = map[key];
+            mostWord = key;
+        }
+    }
+    return mostWord;
+}
--- bot.js
+++ bot.js
@@ -5,74 +5,24 @@
 var Twit = require('./node_modules/twit/lib/twitter');
 
 var Bot = module.exports = function(config) { 
-  this.twit = new Twit(config);
+    this.twit = new Twit(config);
 };
 
-//
-//  post a tweet
-//
+/*
+ * Post a tweet
+ * @param status: the text you will tweet.
+ */
 Bot.prototype.tweet = function (status, callback) {
-  if(typeof status !== 'string') {
-    return callback(new Error('tweet must be of type String'));
-  } else if(status.length > 140) {
-    return callback(new Error('tweet is too long: ' + status.length));
-  }
-  this.twit.post('statuses/update', { status: status }, callback);
+    this.twit.post('statuses/update', { status: status }, callback);
 };
 
-//
-//  choose a random friend of one of your followers, and follow that user
-//
-Bot.prototype.mingle = function (callback) {
-  var self = this;
-  
-  this.twit.get('followers/ids', function(err, reply) {
-      if(err) { return callback(err); }
-      
-      var followers = reply.ids
-        , randFollower  = randIndex(followers);
-        
-      self.twit.get('friends/ids', { user_id: randFollower }, function(err, reply) {
-          if(err) { return callback(err); }
-          
-          var friends = reply.ids
-            , target  = randIndex(friends);
-            
-          self.twit.post('friendships/create', { id: target }, callback); 
-        })
-    })
-};
-
-//
-//  prune your followers list; unfollow a friend that hasn't followed you back
-//
-Bot.prototype.prune = function (callback) {
-  var self = this;
-  
-  this.twit.get('followers/ids', function(err, reply) {
-      if(err) return callback(err);
-      
-      var followers = reply.ids;
-      
-      self.twit.get('friends/ids', function(err, reply) {
-          if(err) return callback(err);
-          
-          var friends = reply.ids
-            , pruned = false;
-          
-          while(!pruned) {
-            var target = randIndex(friends);
-            
-            if(!~followers.indexOf(target)) {
-              pruned = true;
-              self.twit.post('friendships/destroy', { id: target }, callback);   
-            }
-          }
-      });
-  });
-};
-
-function randIndex (arr) {
-  var index = Math.floor(arr.length*Math.random());
-  return arr[index];
-};
+/*
+ * Search for tweets containting a word.
+ * @param word: the word for which you wish to search
+ * @param date: How recent the post should be (YYYY-MM-DD)
+ * @param count: number of posts to return
+ * @return Array of tweets
+ */
+Bot.prototype.searchWord = function(word, date, count, callback) {
+    this.twit.get('search/tweets', { q: '#' + word + ' since:' + date, count: count }, callback);
+}
